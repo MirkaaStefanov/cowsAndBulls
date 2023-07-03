b2e2ed7f67d9a0c0ae8cb09c22cdca307dcc3a15
MirkaaStefanov <mirkanstefanov2007@gmail.com>
2023-07-03 17:58:05 +0300

    Main method for the game, its checking how many digits from the entered number are same with the digits from randomly generated number and then writes how many cows and bulls it have.

--- src/Main.java
+++ src/Main.java
@@ -14,6 +14,30 @@ public class Main {
         }
         return true;
     }
+    public static boolean playGame(String guessNumber, int randomNumber) {
+        String theRandom = String.valueOf(randomNumber);
+        int countCow = 0;
+        int countBull = 0;
+        for (int i = 0; i < theRandom.length(); i++) {
+            for (int j = 0; j < guessNumber.length(); j++) {
+                if (theRandom.charAt(i) == guessNumber.charAt(j)) {
+                    countCow++;
+                    break;
+                }
+            }
+        }
+        for (int i = 0; i < theRandom.length(); i++) {
+            if (theRandom.charAt(i) == guessNumber.charAt(i)) {
+                countBull++;
+            }
+        }
+        System.out.println("Cows: " + countCow + ", Bulls: " + countBull);
+        if (countCow == 4 && countBull == 4) {
+            return true;
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
 
     }
