fcfd66de5165e202034c5acd77a37a0cb64216da
MirkaaStefanov <mirkanstefanov2007@gmail.com>
2023-07-03 18:07:03 +0300

    Menu of the game and 1 player mode

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static boolean doesNumberSuits(String number) {
         int intNumber = Integer.parseInt(number);
@@ -5,7 +8,7 @@ public class Main {
             return false;
         }
         for (int i = 0; i < number.length(); i++) {
-            for (int j = i+1; j < number.length(); j++) {
+            for (int j = i + 1; j < number.length(); j++) {
                 if (number.charAt(i) == number.charAt(j)) {
                     return false;
 
@@ -14,6 +17,7 @@ public class Main {
         }
         return true;
     }
+
     public static boolean playGame(String guessNumber, int randomNumber) {
         String theRandom = String.valueOf(randomNumber);
         int countCow = 0;
@@ -39,6 +43,44 @@ public class Main {
     }
 
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        Random random = new Random();
+        System.out.println("1-> one player, 2-> two players");
+        System.out.print("Enter mode: ");
+        int mode = sc.nextInt();
+        switch (mode) {
+            case 1:
+                int randomNumber;
+                do {
+                    randomNumber = random.nextInt(9000) + 1000;
+                } while (!doesNumberSuits(String.valueOf(randomNumber)));
+                String guessNumber;
+                while (true) {
+                    do {
+                        System.out.print("Enter your guess: ");
+                        guessNumber = sc.next();
+                        if (!doesNumberSuits(guessNumber)) {
+                            System.out.println("Please enter a four-digit number with different digits.");
+                        }
+                    } while (!doesNumberSuits(guessNumber));
+                    while (true) {
+                        boolean resultGame = playGame(guessNumber, randomNumber);
+                        if (resultGame) {
+                            System.out.println("You guessed the number!");
+                            break;
+                        } else {
+                            do {
+                                System.out.print("Enter your guess: ");
+                                guessNumber = sc.next();
+                                if (!doesNumberSuits(guessNumber)) {
+                                    System.out.println("Please enter a four-digit number with different digits.");
+                                }
+                            } while (!doesNumberSuits(guessNumber));
+                        }
+                    }
+                    break;
+                }
+                break;
+        }
     }
 }
